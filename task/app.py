1dd28fd2cd42bd2c8dad79db6ce919c263238b25
Mariano R. <mariano_ramira@epam.com>
2025-12-19 07:22:12 -0300

    feat: app solution

--- task/app.py
+++ task/app.py
@@ -17,6 +17,7 @@ DIAL_ENDPOINT = "https://ai-proxy.lab.epam.com"
 API_KEY = os.getenv('DIAL_API_KEY')
 
 def main():
+    deployment_name = "gpt-4o"
     #TODO:
     # 1. Create UserClient
     # 2. Create DialClient with all tools (WebSearchTool, GetUserByIdTool, SearchUsersTool, CreateUserTool, UpdateUserTool, DeleteUserTool)
@@ -26,7 +27,32 @@ def main():
     #    - Add User message to Conversation
     #    - Call DialClient with conversation history
     #    - Add Assistant message to Conversation and print its content
-    raise NotImplementedError()
+    ## raise NotImplementedError()
+    user_client = UserClient()
+    tools = [
+        WebSearchTool( api_key=API_KEY, endpoint=DIAL_ENDPOINT),
+        GetUserByIdTool(user_client=user_client),
+        SearchUsersTool(user_client=user_client),
+        CreateUserTool(user_client=user_client),
+        UpdateUserTool(user_client=user_client),
+        DeleteUserTool(user_client=user_client)
+    ]
+    dial_client = DialClient(
+        endpoint=DIAL_ENDPOINT,
+        deployment_name=deployment_name,
+        api_key=API_KEY,
+        tools=tools
+    )
+
+    conversation = Conversation()
+    conversation.add_message(Message(role=Role.SYSTEM, content=SYSTEM_PROMPT))
+
+    while True:
+        user_input = input("> ").strip()
+        conversation.add_message(Message(role=Role.USER, content=user_input))
+        assistant_message = dial_client.get_completion(conversation.messages)
+        conversation.add_message(assistant_message)
+        print(assistant_message.content)
 
 
 main()
